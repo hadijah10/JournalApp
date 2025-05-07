84f7b4f56eb89a16f20ad29c5f2d09a10b23d8bb
Atikatu <hadijatumohamadu@gmail.com>
2025-05-07 17:31:21 +0000

    updated

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 const form  = document.getElementById('form')
 const mood = document.getElementById('mood')
 const journal = document.getElementById('content')
+const journalData = document.getElementById('journalContent')
+const edit  = document.getElementById('edit')
+const deletebtn = document.getElementById('delete')
 
-let journalContent = localStorage.getItem('journal') || []
-form.addEventListener('submit',e => {
-    let newJournalContent = [...journalContent,{mood:mood.value,journalContent: journal.value}]
-    localStorage.setItem('journal',JSON.stringify(newJournalContent))
-})
+form.addEventListener('submit',
+   addjournal)
+
+function addjournal(e){
+    e.preventDefault()
+    let text = journal.value.trim()
+    if(text){
+        let localStorageRaw = localStorage.getItem('journal');
+        let localStorageData = localStorageRaw ? JSON.parse(localStorageRaw) : [];
+        let newData = { mood: mood.value, journalContent: text};
+        let newLocalStorageData = [...localStorageData, newData];
+        localStorage.setItem('journal', JSON.stringify(newLocalStorageData));
+         updateUI()
+         form.reset()
+    }
+}
+
 
 function updateUI(){
+    let localStorageRaw = localStorage.getItem('journal');
+    let localStorageData = localStorageRaw ? JSON.parse(localStorageRaw) : [];
+    let child = ''
+    localStorageData.map((data,key) => {
+        child += `<div class = 'list ${key}'>
+        <div class='moood'>${data.mood}</div>
+        <p class='journalText'>${data.journalContent}</p>
+        <button type='button' id='edit' onClick= 'editJournal(${key})'>Edit</button>
+        <button type='button' id='delete' onClick='deleteJournal(${key})'>Delete</button>
+        </div>`
      
-     console.log(journalContent)
-
+    })
+    journalData.innerHTML = child
     }
+updateUI()
 
-   updateUI()
+function editJournal(index){
+    let journalList = JSON.parse(localStorage.getItem('journal'));
+    let newJournalList = journalList.filter((data,key)=> {
+
+    })
+}
+function deleteJournal(index){
+    let journalList = JSON.parse(localStorage.getItem('journal'));
+    console.log(journalList);
+    let newJournalList = journalList.filter((data,key)=> {
+        key !== parseInt(index)
+    })
+    console.log(newJournalList)
+    localStorage.setItem('journal',JSON.stringify(newJournalList))
+    updateUI()
+}
